9e7cd59260b4d982d8d73f6ebffef0f4cc540d96
romanhaa <roman.hillje@googlemail.com>
2019-09-17 09:22:01 +0200

    Save function to send enrichR query in separate file.

--- R/getEnrichedPathways.R
+++ R/getEnrichedPathways.R
@@ -96,7 +96,7 @@ getEnrichedPathways <- function(
               dplyr::select('gene') %>%
               t() %>%
               as.vector() %>%
-              enrichr(databases = databases, URL_API = URL_API)
+              send_enrichr_query(databases = databases, URL_API = URL_API)
           )
         }
         #
@@ -165,7 +165,7 @@ getEnrichedPathways <- function(
               dplyr::select('gene') %>%
               t() %>%
               as.vector() %>%
-              enrichr(databases = databases, URL_API = URL_API)
+              send_enrichr_query(databases = databases, URL_API = URL_API)
           )
         }
         #
@@ -210,51 +210,4 @@ getEnrichedPathways <- function(
   ## return Seurat object
   ##--------------------------------------------------------------------------##
   return(temp_seurat)
-}
-
-
-#' Gene enrichment using Enrichr.
-#' @title Gene enrichment using Enrichr.
-#' @description Gene enrichment using Enrichr.
-#' @param genes Gene names or dataframe of gene names in first column and a
-#' score between 0 and 1 in the other.
-#' @param databases Databases to search.
-#' @param API_url URL to send requests to (Enrichr API).
-#' See http://amp.pharm.mssm.edu/Enrichr/ for available databases.
-#' @return Returns a data frame of enrichment terms, p-values, ...
-#' @author Wajid Jawaid, modified by Roman Hillje
-enrichr <- function(
-  genes,
-  databases = NULL,
-  URL_API = NULL
-) {
-  if (is.vector(genes) & ! all(genes == '') & length(genes) != 0) {
-    temp <- httr::POST(
-      url = URL_API,
-      body = list(list = paste(genes, collapse = '\n'))
-    )
-  } else if (is.data.frame(genes)) {
-    temp <- httr::POST(
-      url = URL_API,
-      body = list(list = paste(paste(genes[,1], genes[,2], sep = ','), collapse = '\n'))
-    )
-  } else {
-    warning('genes must be a non-empty vector of gene names or a dataframe with genes and score.')
-  }
-  httr::GET(url = 'http://amp.pharm.mssm.edu/Enrichr/share')
-  dfSAF <- options()$stringsAsFactors
-  options(stringsAsFactors = FALSE)
-  result <- future.apply::future_sapply(databases, USE.NAMES = TRUE, simplify = FALSE, function(x) {
-    r <- httr::GET(
-      url = 'http://amp.pharm.mssm.edu/Enrichr/export',
-      query = list(file = 'API', backgroundType = x)
-    )
-    r <- gsub('&#39;', "'", intToUtf8(r$content))
-    tc <- textConnection(r)
-    r <- read.table(tc, sep = '\t', header = TRUE, quote = '', comment.char = '')
-    close(tc)
-    return(r)
-  })
-  options(stringsAsFactors = dfSAF)
-  return(result)
 }
